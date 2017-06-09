f530e7372ed1015415083da46a32d244e14eb1a5
sevivon <sevivon64@gmail.com>
2017-06-10 00:04:14 +0200

    Ajout variable bloc pour damier

--- structures/Damier.java
+++ structures/Damier.java
@@ -9,13 +9,16 @@ class Damier
 	private int largeur;
 	private int hauteur;
 
+	private String bloc;
+
 	private String couleur1;
 	private String couleur2;
 
-	Damier ( int largeur, int hauteur, String couleur1, String couleur2 )
+	Damier ( int largeur, int hauteur, String bloc, String couleur1, String couleur2 )
 	{
 		this.largeur  = largeur;
 		this.hauteur  = hauteur;
+		this.bloc     = bloc;
 		this.couleur1 = couleur1;
 		this.couleur2 = couleur2;
 	}
@@ -25,10 +28,10 @@ class Damier
 		try
 		{
 			PrintWriter pw = new PrintWriter(fichier);
-			
+
 			for ( int dx = 0 ; dx < this.largeur ; dx++ )
 				for ( int dz = 0 ; dz < this.hauteur ; dz++ )
-					pw.println(String.format("setblock ~%d ~ ~%d wool color=%s",dx,dz,(dx+dz)%2==0?this.couleur1:this.couleur2));
+					pw.println(String.format("setblock ~%d ~ ~%d %s color=%s",dx,dz,this.bloc,(dx+dz)%2==0?this.couleur1:this.couleur2));
 
 			pw.close();
 		}
@@ -40,13 +43,13 @@ class Damier
 
 	public static void main ( String... argv )
 	{
-		Damier echiquier = new Damier(8,8,"white","black");
+		Damier echiquier = new Damier(8,8,"concrete","white","black");
 		echiquier.genererFonction("fonctions/echiquier.mcfunction");
 
-		Damier damier = new Damier(10,10,"white","black");
+		Damier damier = new Damier(10,10,"concrete","white","black");
 		damier.genererFonction("fonctions/plateauDames.mcfunction");
 
-		Damier damier50 = new Damier(50,50,"lime","red");
+		Damier damier50 = new Damier(50,50,"wool","lime","red");
 		damier50.genererFonction("fonctions/damier50.mcfunction");
 	}
 }
